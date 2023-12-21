53edf042b2357e68a2c8bee8dba511f7ef209d17
Matthew Olson <o.matt432@gmail.com>
2023-12-21 14:10:10 -0600

    Refactor UI and constants

--- constants.py
+++ constants.py
@@ -1,9 +1,11 @@
 THEME_BG_COLOR = "#f1d9c1"
+LIGHT_BG_COLOR = "#f8f8f8"
+ENTRY_BG_COLOR = "#f2f2f2"
 TITLE_COLOR = "#7b4b94"
 LABEL_COLOR = "#767BB6"
 VALUE_COLOR = "#31231e"
 
-TITLE_FONT = ("Courier", 21, "bold")
+TITLE_FONT = ("Courier", 24, "bold")
 LABEL_FONT = ("Arial", 16, "bold")
 VALUE_FONT = ("Arial", 14, "italic")
 DT_FONT = ("Arial", 12, "normal")
--- ui.py
+++ ui.py
@@ -23,7 +23,7 @@ class ForecastInterface():
 
         self.city_label = tkinter.Label(text="Country:", 
                                         fg=LABEL_COLOR, 
-                                        bg=THEME_BG_COLOR, 
+                                        bg=THEME_BG_COLOR,
                                         font=LABEL_FONT)
         self.city_label.grid(row=2, column=0, sticky=tkinter.E, pady=(0, 10))
 
@@ -31,22 +31,87 @@ class ForecastInterface():
         city = tkinter.StringVar()
         country = tkinter.StringVar()
 
-        self.city_entry = tkinter.Entry(textvariable=city, font=ENTRY_FONT)
+        self.city_entry = tkinter.Entry(textvariable=city, font=ENTRY_FONT, bg=ENTRY_BG_COLOR)
         self.city_entry.grid(row=1, column=1, sticky=tkinter.EW)
         self.city_entry.focus()
 
-        self.country_entry = tkinter.Entry(textvariable=country, font=ENTRY_FONT)
+        self.country_entry = tkinter.Entry(textvariable=country, font=ENTRY_FONT, bg=ENTRY_BG_COLOR)
         self.country_entry.grid(row=2, column=1, sticky=tkinter.EW, pady=(0, 10))
 
-        # tkinter Buttons
+        # tkinter - button
         self.get_forecast_btn = tkinter.Button(text="Get Forecast", 
                                                highlightthickness=0, 
                                                command=lambda: self.fill_forecast_info(city.get(), country.get()),
                                                font=BTN_FONT)
-        self.get_forecast_btn.grid(row=3,column=0, columnspan=3, sticky=tkinter.EW, padx=(20, 20))
+        self.get_forecast_btn.grid(row=3,column=0, columnspan=3, sticky=tkinter.EW, padx=(20, 20), pady=(0, 20))
 
+        # tkinter - Forecast canvas'
+            # tkinter Forecast One - canvas
+        self.forecast_one_canvas = tkinter.Canvas(width=320, height=140, bg=LIGHT_BG_COLOR)
 
+        self.fcast_one_condition = self.forecast_one_canvas.create_text(160, 30, width=300,
+                                                                           text="Condition: ----",
+                                                                           fill=VALUE_COLOR,
+                                                                           font=VALUE_FONT)
+        self.fcast_one_clouds = self.forecast_one_canvas.create_text(160, 70, width=300,
+                                                                           text="Cloud Coverage: ----",
+                                                                           fill=VALUE_COLOR,
+                                                                           font=VALUE_FONT)
+        self.fcast_one_dt = self.forecast_one_canvas.create_text(160, 110, width=300,
+                                                                           text="Date : Time",
+                                                                           fill=VALUE_COLOR,
+                                                                           font=DT_FONT)
+        self.forecast_one_canvas.grid(row=4, column=0, columnspan=3)
+            # tkinter Forecast Two - canvas
+        self.forecast_two_canvas = tkinter.Canvas(width=320, height=140, bg="white")
 
+        self.fcast_two_condition = self.forecast_two_canvas.create_text(160, 30, width=300,
+                                                                           text="Condition: ----",
+                                                                           fill=VALUE_COLOR,
+                                                                           font=VALUE_FONT)
+        self.fcast_two_clouds = self.forecast_two_canvas.create_text(160, 70, width=300,
+                                                                           text="Cloud Coverage: ----",
+                                                                           fill=VALUE_COLOR,
+                                                                           font=VALUE_FONT)
+        self.fcast_two_dt = self.forecast_two_canvas.create_text(160, 110, width=300,
+                                                                           text="Date : Time",
+                                                                           fill=VALUE_COLOR,
+                                                                           font=DT_FONT)
+        self.forecast_two_canvas.grid(row=5, column=0, columnspan=3)
+            # tkinter Forecast Three - canvas
+        self.forecast_three_canvas = tkinter.Canvas(width=320, height=140, bg="white")
+
+        self.fcast_three_condition = self.forecast_three_canvas.create_text(160, 30, width=300,
+                                                                           text="Condition: ----",
+                                                                           fill=VALUE_COLOR,
+                                                                           font=VALUE_FONT)
+        self.fcast_three_clouds = self.forecast_three_canvas.create_text(160, 70, width=300,
+                                                                           text="Cloud Coverage: ----",
+                                                                           fill=VALUE_COLOR,
+                                                                           font=VALUE_FONT)
+        self.fcast_three_dt = self.forecast_three_canvas.create_text(160, 110, width=300,
+                                                                           text="Date : Time",
+                                                                           fill=VALUE_COLOR,
+                                                                           font=DT_FONT)
+        self.forecast_three_canvas.grid(row=6, column=0, columnspan=3)
+            # tkinter Forecast Four - canvas
+        self.forecast_four_canvas = tkinter.Canvas(width=320, height=140, bg="white")
+
+        self.fcast_four_condition = self.forecast_four_canvas.create_text(160, 30, width=300,
+                                                                           text="Condition: ----",
+                                                                           fill=VALUE_COLOR,
+                                                                           font=VALUE_FONT)
+        self.fcast_four_clouds = self.forecast_four_canvas.create_text(160, 70, width=300,
+                                                                           text="Cloud Coverage: ----",
+                                                                           fill=VALUE_COLOR,
+                                                                           font=VALUE_FONT)
+        self.fcast_four_dt = self.forecast_four_canvas.create_text(160, 110, width=300,
+                                                                           text="Date : Time",
+                                                                           fill=VALUE_COLOR,
+                                                                           font=DT_FONT)
+        self.forecast_four_canvas.grid(row=7, column=0, columnspan=3)
+
+        # tkinter mainloop
         self.window.mainloop()
 
     def fill_forecast_info(self, city:str, country:str) -> None:
